a9ceb296e46ca1e8b384bfe66142ffe5e0344800
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2021-05-22 11:30:46 +0800

    Add files via upload

--- centernet.py
+++ centernet.py
@@ -1,5 +1,6 @@
 import colorsys
 import os
+import time
 
 import numpy as np
 import tensorflow as tf
@@ -7,6 +8,7 @@ from PIL import ImageDraw, ImageFont
 
 from nets.centernet import centernet
 from utils.utils import centernet_correct_boxes, letterbox_image, nms
+
 
 def preprocess_image(image):
     mean    = [0.40789655, 0.44719303, 0.47026116]
@@ -24,9 +26,10 @@ class CenterNet(object):
     _defaults = {
         "model_path"        : 'model_data/centernet_resnet50_voc.h5',
         "classes_path"      : 'model_data/voc_classes.txt',
+        "backbone"          : 'resnet50',
         # "model_path"        : 'model_data/centernet_hourglass_coco.h5',
         # "classes_path"      : 'model_data/coco_classes.txt',
-        "backbone"          : 'resnet50',
+        # "backbone"          : 'hourglass',
         "input_shape"       : [512,512,3],
         "confidence"        : 0.3,
         # backbone为resnet50时建议设置为True
@@ -193,3 +196,67 @@ class CenterNet(object):
             draw.text(text_origin, str(label,'UTF-8'), fill=(0, 0, 0), font=font)
             del draw
         return image
+
+    def get_FPS(self, image, test_interval):
+        #---------------------------------------------------------#
+        #   在这里将图像转换成RGB图像，防止灰度图在预测时报错。
+        #---------------------------------------------------------#
+        image = image.convert('RGB')
+
+        image_shape = np.array(np.shape(image)[0:2])
+        #---------------------------------------------------------#
+        #   给图像增加灰条，实现不失真的resize
+        #---------------------------------------------------------#
+        crop_img = letterbox_image(image, [self.input_shape[0],self.input_shape[1]])
+        #----------------------------------------------------------------------------------#
+        #   将RGB转化成BGR，这是因为原始的centernet_hourglass权值是使用BGR通道的图片训练的
+        #----------------------------------------------------------------------------------#
+        photo = np.array(crop_img,dtype = np.float32)[:,:,::-1]
+        #-----------------------------------------------------------#
+        #   图片预处理，归一化。获得的photo的shape为[1, 512, 512, 3]
+        #-----------------------------------------------------------#
+        photo = np.reshape(preprocess_image(photo), [1, self.input_shape[0], self.input_shape[1], self.input_shape[2]])
+
+        preds = self.get_pred(photo).numpy()
+        
+        if self.nms:
+            preds = np.array(nms(preds, self.nms_threhold))
+
+        if len(preds[0])>0:
+            preds[0][:, 0:4] = preds[0][:, 0:4] / (self.input_shape[0] / 4)
+            
+            det_label = preds[0][:, -1]
+            det_conf = preds[0][:, -2]
+            det_xmin, det_ymin, det_xmax, det_ymax = preds[0][:, 0], preds[0][:, 1], preds[0][:, 2], preds[0][:, 3]
+
+            top_indices = [i for i, conf in enumerate(det_conf) if conf >= self.confidence]
+            top_conf = det_conf[top_indices]
+            top_label_indices = det_label[top_indices].tolist()
+            top_xmin, top_ymin, top_xmax, top_ymax = np.expand_dims(det_xmin[top_indices],-1),np.expand_dims(det_ymin[top_indices],-1),np.expand_dims(det_xmax[top_indices],-1),np.expand_dims(det_ymax[top_indices],-1)
+            
+            boxes = centernet_correct_boxes(top_ymin,top_xmin,top_ymax,top_xmax,np.array([self.input_shape[0],self.input_shape[1]]),image_shape)
+
+        t1 = time.time()
+        for _ in range(test_interval):
+            preds = self.get_pred(photo).numpy()
+            
+            if self.nms:
+                preds = np.array(nms(preds, self.nms_threhold))
+
+            if len(preds[0])>0:
+                preds[0][:, 0:4] = preds[0][:, 0:4] / (self.input_shape[0] / 4)
+                
+                det_label = preds[0][:, -1]
+                det_conf = preds[0][:, -2]
+                det_xmin, det_ymin, det_xmax, det_ymax = preds[0][:, 0], preds[0][:, 1], preds[0][:, 2], preds[0][:, 3]
+
+                top_indices = [i for i, conf in enumerate(det_conf) if conf >= self.confidence]
+                top_conf = det_conf[top_indices]
+                top_label_indices = det_label[top_indices].tolist()
+                top_xmin, top_ymin, top_xmax, top_ymax = np.expand_dims(det_xmin[top_indices],-1),np.expand_dims(det_ymin[top_indices],-1),np.expand_dims(det_xmax[top_indices],-1),np.expand_dims(det_ymax[top_indices],-1)
+                
+                boxes = centernet_correct_boxes(top_ymin,top_xmin,top_ymax,top_xmax,np.array([self.input_shape[0],self.input_shape[1]]),image_shape)
+
+        t2 = time.time()
+        tact_time = (t2 - t1) / test_interval
+        return tact_time
--- train.py
+++ train.py
@@ -1,17 +1,78 @@
+from functools import partial
+
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.callbacks import (EarlyStopping, ReduceLROnPlateau,
                                         TensorBoard)
+from tqdm import tqdm
 
-from nets.centernet_training import Generator, LossHistory
 from nets.centernet import centernet
+from nets.centernet_training import Generator, LossHistory
 from utils.utils import ModelCheckpoint
 
 gpus = tf.config.experimental.list_physical_devices(device_type='GPU')
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
 
+# 防止bug
+def get_train_step_fn():
+    @tf.function
+    def train_step(batch_images, batch_hms, batch_whs, batch_regs, batch_reg_masks, batch_indices, net, optimizer):
+        with tf.GradientTape() as tape:
+            loss_value = net([batch_images, batch_hms, batch_whs, batch_regs, batch_reg_masks, batch_indices], training=True)
+        grads = tape.gradient(loss_value, net.trainable_variables)
+        optimizer.apply_gradients(zip(grads, net.trainable_variables))
+        return loss_value
+    return train_step
+
+@tf.function
+def val_step(batch_images, batch_hms, batch_whs, batch_regs, batch_reg_masks, batch_indices, net, optimizer):
+    loss_value = net([batch_images, batch_hms, batch_whs, batch_regs, batch_reg_masks, batch_indices])
+    return loss_value
+
+def fit_one_epoch(net, optimizer, epoch, epoch_size, epoch_size_val, gen, genval, Epoch, train_step=None):
+
+    total_loss = 0
+    val_loss = 0
+    print('Start Train')
+    with tqdm(total=epoch_size,desc=f'Epoch {epoch + 1}/{Epoch}',postfix=dict,mininterval=0.3) as pbar:
+        for iteration, batch in enumerate(gen):
+            if iteration>=epoch_size:
+                break
+            batch = [tf.convert_to_tensor(part) for part in batch]
+            batch_images, batch_hms, batch_whs, batch_regs, batch_reg_masks, batch_indices = batch
+
+            loss_value = train_step(batch_images, batch_hms, batch_whs, batch_regs, batch_reg_masks, batch_indices, net, optimizer)
+            total_loss += loss_value
+
+            pbar.set_postfix(**{'total_loss'        : float(total_loss) / (iteration + 1), 
+                                'lr'                : optimizer._decayed_lr(tf.float32).numpy()})
+            pbar.update(1)
+
+    print('Start Validation')
+    with tqdm(total=epoch_size_val, desc=f'Epoch {epoch + 1}/{Epoch}',postfix=dict,mininterval=0.3) as pbar:
+        for iteration, batch in enumerate(genval):
+            if iteration>=epoch_size_val:
+                break
+            # 计算验证集loss
+            batch = [tf.convert_to_tensor(part) for part in batch]
+            batch_images, batch_hms, batch_whs, batch_regs, batch_reg_masks, batch_indices = batch
+
+            loss_value = val_step(batch_images, batch_hms, batch_whs, batch_regs, batch_reg_masks, batch_indices, net, optimizer)
+            val_loss += loss_value
+
+            # 更新验证集loss
+            pbar.set_postfix(**{'total_loss': float(val_loss)/ (iteration + 1)})
+            pbar.update(1)
+
+    logs = {'loss': total_loss.numpy()/(epoch_size+1), 'val_loss': val_loss.numpy()/(epoch_size_val+1)}
+    loss_history.on_epoch_end([], logs)
+    print('Finish Validation')
+    print('\nEpoch:'+ str(epoch+1) + '/' + str(Epoch))
+    print('Total Loss: %.4f || Val Loss: %.4f ' % (total_loss/(epoch_size+1),val_loss/(epoch_size_val+1)))
+    net.save_weights('logs/Epoch%d-Total_Loss%.4f-Val_Loss%.4f.h5'%((epoch+1),total_loss/(epoch_size+1),val_loss/(epoch_size_val+1)))
+      
 #---------------------------------------------------#
 #   获得类
 #---------------------------------------------------#
@@ -27,6 +88,10 @@ def get_classes(classes_path):
 #   https://www.bilibili.com/video/BV1zE411u7Vw
 #----------------------------------------------------#
 if __name__ == "__main__": 
+    #----------------------------------------------------#
+    #   是否使用eager模式训练
+    #----------------------------------------------------#
+    eager = True
     #-----------------------------#
     #   图片的大小
     #-----------------------------#
@@ -114,31 +179,49 @@ if __name__ == "__main__":
     #------------------------------------------------------#
     if True:
         Lr              = 1e-3
-        Batch_size      = 4
+        Batch_size      = 8
         Init_Epoch      = 0
         Freeze_Epoch    = 50
-
-        gen = Generator(Batch_size, lines[:num_train], lines[num_train:], input_shape, num_classes)
-
-        model.compile(
-            loss={'centernet_loss': lambda y_true, y_pred: y_pred},
-            optimizer=keras.optimizers.Adam(Lr)
-        )
-
+        
         epoch_size      = num_train // Batch_size
         epoch_size_val  = num_val // Batch_size
 
         if epoch_size == 0 or epoch_size_val == 0:
             raise ValueError("数据集过小，无法进行训练，请扩充数据集。")
 
-        model.fit(gen.generate(True), 
-                steps_per_epoch=epoch_size,
-                validation_data=gen.generate(False),
-                validation_steps=epoch_size_val,
-                epochs=Freeze_Epoch, 
-                verbose=1,
-                initial_epoch=Init_Epoch,
-                callbacks=[logging, checkpoint, reduce_lr, early_stopping, loss_history])
+        print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, Batch_size))
+        if eager:
+            generator       = Generator(Batch_size, lines[:num_train], lines[num_train:], input_shape, num_classes)
+            
+            gen             = tf.data.Dataset.from_generator(partial(generator.generate, train = True, eager = True), (tf.float32, tf.float32, tf.float32, tf.float32, tf.float32, tf.float32))
+            gen_val         = tf.data.Dataset.from_generator(partial(generator.generate, train = False, eager = True), (tf.float32, tf.float32, tf.float32, tf.float32, tf.float32, tf.float32))
+
+            gen             = gen.shuffle(buffer_size=Batch_size).prefetch(buffer_size=Batch_size)
+            gen_val         = gen_val.shuffle(buffer_size=Batch_size).prefetch(buffer_size=Batch_size)
+
+            lr_schedule     = tf.keras.optimizers.schedules.ExponentialDecay(
+                initial_learning_rate=Lr, decay_steps=epoch_size, decay_rate=0.92, staircase=True
+            )
+            optimizer       = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
+
+            for epoch in range(Init_Epoch,Freeze_Epoch):
+                fit_one_epoch(model, optimizer, epoch, epoch_size, epoch_size_val, gen, gen_val, Freeze_Epoch, get_train_step_fn())
+
+        else:
+            gen = Generator(Batch_size, lines[:num_train], lines[num_train:], input_shape, num_classes)
+            model.compile(
+                loss={'centernet_loss': lambda y_true, y_pred: y_pred},
+                optimizer=keras.optimizers.Adam(Lr)
+            )
+            model.fit(gen.generate(True), 
+                    steps_per_epoch=epoch_size,
+                    validation_data=gen.generate(False),
+                    validation_steps=epoch_size_val,
+                    epochs=Freeze_Epoch, 
+                    verbose=1,
+                    initial_epoch=Init_Epoch,
+                    callbacks=[logging, checkpoint, reduce_lr, early_stopping, loss_history])
+
 
     for i in range(freeze_layer):
         model.layers[i].trainable = True
@@ -149,24 +232,42 @@ if __name__ == "__main__":
         Freeze_Epoch    = 50
         Epoch           = 100
         
-        gen             = Generator(Batch_size, lines[:num_train], lines[num_train:], input_shape, num_classes)
-
-        model.compile(
-            loss={'centernet_loss': lambda y_true, y_pred: y_pred},
-            optimizer=keras.optimizers.Adam(Lr)
-        )
-
-        epoch_size = num_train // Batch_size
-        epoch_size_val = num_val // Batch_size
+        epoch_size      = num_train // Batch_size
+        epoch_size_val  = num_val // Batch_size
 
         if epoch_size == 0 or epoch_size_val == 0:
             raise ValueError("数据集过小，无法进行训练，请扩充数据集。")
 
-        model.fit(gen.generate(True), 
-                steps_per_epoch=epoch_size,
-                validation_data=gen.generate(False),
-                validation_steps=epoch_size_val,
-                epochs=Epoch, 
-                verbose=1,
-                initial_epoch=Freeze_Epoch,
-                callbacks=[logging, checkpoint, reduce_lr, early_stopping, loss_history])
+        print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, Batch_size))
+        if eager:
+            generator       = Generator(Batch_size, lines[:num_train], lines[num_train:], input_shape, num_classes)
+            
+            gen             = tf.data.Dataset.from_generator(partial(generator.generate, train = True, eager = True), (tf.float32, tf.float32, tf.float32, tf.float32, tf.float32, tf.float32))
+            gen_val         = tf.data.Dataset.from_generator(partial(generator.generate, train = False, eager = True), (tf.float32, tf.float32, tf.float32, tf.float32, tf.float32, tf.float32))
+
+            gen             = gen.shuffle(buffer_size=Batch_size).prefetch(buffer_size=Batch_size)
+            gen_val         = gen_val.shuffle(buffer_size=Batch_size).prefetch(buffer_size=Batch_size)
+            
+            lr_schedule     = tf.keras.optimizers.schedules.ExponentialDecay(
+                initial_learning_rate=Lr, decay_steps=epoch_size, decay_rate=0.92, staircase=True
+            )
+
+            optimizer       = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
+            
+            for epoch in range(Freeze_Epoch,Epoch):
+                fit_one_epoch(model, optimizer, epoch, epoch_size, epoch_size_val, gen, gen_val, Epoch, get_train_step_fn())
+
+        else:
+            gen = Generator(Batch_size, lines[:num_train], lines[num_train:], input_shape, num_classes)
+            model.compile(
+                loss={'centernet_loss': lambda y_true, y_pred: y_pred},
+                optimizer=keras.optimizers.Adam(Lr)
+            )
+            model.fit(gen.generate(True), 
+                    steps_per_epoch=epoch_size,
+                    validation_data=gen.generate(False),
+                    validation_steps=epoch_size_val,
+                    epochs=Epoch, 
+                    verbose=1,
+                    initial_epoch=Freeze_Epoch,
+                    callbacks=[logging, checkpoint, reduce_lr, early_stopping, loss_history])
